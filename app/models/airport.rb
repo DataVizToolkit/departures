2523a49a8f5c413320b39ea85c9a2f2e2b6a3d50
Barrett Clark <barrett@websitesbybarrett.com>
2016-05-29 10:32:03 -0500

    Find items near a point scope

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,22 @@
 class Airport < ActiveRecord::Base
+  scope :close_to, -> (lon, lat, distance_in_meters = 200 * 1609.34) {
+    select(sanitize_sql_array([%{
+      ST_Distance(
+        lonlat,
+        ST_GeomFromText('POINT(? ?)',4326)
+      ) AS distance_meters,
+      *
+    }, lon, lat])).
+    where([%{
+      ST_DWithin(
+        lonlat,
+        ST_GeographyFromText('SRID=4326;POINT(? ?)'),
+        ?
+      )
+    }, lon, lat, distance_in_meters]).
+    order("1")
+  }
+
   module Factories
     GEO = RGeo::Geographic.spherical_factory(:srid => 4326)
   end
